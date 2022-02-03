7d851f4f7985bb72aa85fdf7fb46249d59111563
Josh Salsbury <joshuajsalsbury@gmail.com>
2022-02-03 15:10:36 -0600

    part-supply.js; completed 1-6, chg 4,5 (corrected console.log output), started stretch 7

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -46,7 +46,18 @@ else {
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop.
 console.log('7. Showing supplyChanges with "for of" loop');
-
+let y=0;
+for (y of supplyChanges){
+  if (supplyChanges>0){
+    console.log('Added', y, 'parts.');
+  }
+  else if (y===0){
+    console.log('No change.');
+  }
+  else {
+    console.log('Removed', y*-1, 'parts.')
+  }
+}
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
